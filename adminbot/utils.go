5a0a832062bcb5067e87b7268d04d1d0e4df1ead
yichi170 <ilee900620@gmail.com>
2022-07-19 23:55:14 +0800

    [admin] fix prompt & add getall

--- adminbot/utils.go
+++ adminbot/utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,16 +17,21 @@ import (
 func StrParser(request string) *linebot.TextMessage {
 	words := strings.Fields(request)
 	response := "請按照以下格式\n「商品名 單位 (斤/盤) 價錢」\n並以空白隔開\n例：鮭魚 斤 750\n\n"
-	response += "或輸入「刪除 商品名」\n刪除商品資訊"
+	response += "或輸入「刪除 商品名」\n刪除商品資訊\n\n"
+	response += "或輸入「查看」\n查看全部商品\n\n"
+	if words[0] == "刪除" {
+		response = deletefish(words[1])
+	} else if words[0] == "查看" {
+		response = getallfish()
+	}
+
 	if len(words) < 3 {
 		return linebot.NewTextMessage(response)
 	}
 	if _, err := strconv.Atoi(words[2]); err != nil {
 		response = "價錢需為數字"
-	} else if words[0] != "刪除" {
-		response = postfish(words)
 	} else {
-		response = deletefish(words[1])
+		response = postfish(words)
 	}
 	return linebot.NewTextMessage(response)
 }
@@ -77,6 +83,31 @@ func deletefish(fish string) string {
 	if err != nil {
 		retmsg = "更新資料時發生問題"
 		log.Fatalln(err)
+	}
+	return retmsg
+}
+
+func getallfish() string {
+	retmsg := ""
+	address := os.Getenv("API_ADDRESS") + "/fish"
+	resp, err := http.Get(address)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var fishmpslice []map[string]interface{}
+	if err := json.Unmarshal(body, &fishmpslice); err != nil {
+		log.Fatalln(err)
+	}
+	for i, fishmp := range fishmpslice {
+		retmsg += fishmp["name"].(string) + "一" + fishmp["unit"].(string) + fishmp["price"].(string) + "元"
+		if i < len(fishmpslice)-1 {
+			retmsg += "\n"
+		}
 	}
 	return retmsg
 }
