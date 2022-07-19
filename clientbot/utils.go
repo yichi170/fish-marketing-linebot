db58c41ff08b7a12c0f4aec44172c78469eace64
yichi170 <ilee900620@gmail.com>
2022-07-20 01:08:26 +0800

    [client] test flex message

--- clientbot/utils.go
+++ clientbot/utils.go
@@ -12,17 +12,23 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-func StrParser(request string) *linebot.TextMessage {
+func StrParser(request string) *linebot.FlexMessage {
 	words := strings.Fields(request)
 	response := "請輸入'看魚價'來看今日魚價哦"
+	var ret []byte
 	switch words[0] {
 	case "看魚價":
-		response = getallfish()
+		response = getallfish(false)
+		ret = []byte(getallfish(true))
 	}
-	return linebot.NewTextMessage(response)
+	container, err := linebot.UnmarshalFlexMessageJSON(ret)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return linebot.NewFlexMessage(response, container)
 }
 
-func getallfish() string {
+func getallfish(goodlook bool) string {
 	retmsg := ""
 	address := os.Getenv("API_ADDRESS") + "/fish"
 	resp, err := http.Get(address)
@@ -38,11 +44,42 @@ func getallfish() string {
 	if err := json.Unmarshal(body, &fishmpslice); err != nil {
 		log.Fatalln(err)
 	}
+
+	if goodlook {
+		retmsg = "{\"type\": \"carousel\", \"contents\": ["
+	}
+
 	for i, fishmp := range fishmpslice {
-		retmsg += fishmp["name"].(string) + "一" + fishmp["unit"].(string) + fishmp["price"].(string) + "元"
-		if i < len(fishmpslice)-1 {
-			retmsg += "\n"
+		if goodlook {
+			retmsg += newflexmsg(fishmp)
+			if i < len(fishmpslice)-1 {
+				retmsg += ","
+			}
+		} else {
+			retmsg += fishmp["name"].(string) + "一" + fishmp["unit"].(string) + fishmp["price"].(string) + "元"
+			if i < len(fishmpslice)-1 {
+				retmsg += "\n"
+			}
 		}
 	}
+	if goodlook {
+		retmsg += "]}"
+	}
 	return retmsg
+}
+
+func newflexmsg(fishmp map[string]interface{}) string {
+	jsonFile, err := os.Open("flex-msg.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	str := string(byteValue)
+	str = strings.Replace(str, "name", fishmp["name"].(string), 1)
+	str = strings.Replace(str, "price", fishmp["price"].(string), 1)
+	str = strings.Replace(str, "unit", fishmp["unit"].(string), 1)
+
+	return str
 }
